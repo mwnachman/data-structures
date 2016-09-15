4ea067b7f21c34c3bd3387659459e370fc63184a
mwnachman <mwnachman@gmail.com>
2016-09-14 19:53:14 -0700

    Remove console.log

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -27,15 +27,15 @@ queueMethods.dequeue = function() {
   if (this.counter > 0) {
     this.counter--;
   }
-  // console.log('obj', this.storage);
+
   var numberedKeys = [];
   for (var key in this.storage) {
     numberedKeys.push(Number(key));
   }
-  console.log(numberedKeys);
+
   var keyToRemove = numberedKeys.sort();
   var keyToRemove = keyToRemove[0];
-  console.log('key', keyToRemove);
+
   var toDequeue = this.storage[keyToRemove];
   delete this.storage[keyToRemove];
   return toDequeue;
@@ -46,45 +46,3 @@ queueMethods.size = function() {
 };
 
 
-
-// var Queue = function() {
-//   var someInstance = {};
-
-//   var counter = 0;
-
-//   var key = 0;
-//   // Use an object with numeric keys to store values
-//   var storage = {};
-
-//   // Implement the methods below
-
-//   someInstance.enqueue = function(value) {
-//     storage[key] = value;
-//     key++;
-//     counter++;
-//   };
-
-//   someInstance.dequeue = function() {
-
-//     if (counter > 0) {  
-//       counter--;
-//     }
-
-//     var numberedKeys = [];
-//     for (var key in storage) {
-//       numberedKeys.push(Number(key));
-//     }
-//     var keyToRemove = numberedKeys.sort();
-//     var keyToRemove = keyToRemove[0];
-//     console.log(keyToRemove);
-//     var valToDequeue = storage[keyToRemove];
-//     delete storage[keyToRemove];
-//     return valToDequeue;
-//   };
-
-//   someInstance.size = function() {
-//     return counter;
-//   };
-
-//   return someInstance;
-// };
