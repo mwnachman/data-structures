6850fa359f5ddc7d632db9427d359d42253ab025
mwnachman <mwnachman@gmail.com>
2016-09-15 15:03:18 -0700

    Complete tree functional share pattern

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,10 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
+  _.extend(newTree, treeMethods);
+
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +13,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+ 
+  this.children.push(Tree(value));
+
 };
 
 treeMethods.contains = function(target) {
+
+  var result = false;
+
+  var searchNodes = function(node) {
+    if (node.value === target) {
+      result = true;
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      searchNodes(node.children[i]);
+    }
+  };
+
+  searchNodes(this);
+
+  return result;
+
+
 };
 
 
