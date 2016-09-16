2de1f59803a3f6f4216f15f255a00d24a26599ff
mwnachman <mwnachman@gmail.com>
2016-09-16 10:42:07 -0700

    Implement set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,29 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage === undefined) {
+    this._storage = [item];
+  } else {
+    //potentially add an iteration to see if it's already in there before adding
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
