55a3aafb82b7269a846e3b9b1ded22364243f7ba
mwnachman <mwnachman@gmail.com>
2016-09-17 17:06:37 -0700

    New version of recusion for the contains method of BinarySearchTree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -8,47 +8,6 @@ var BinarySearchTree = function(value) {
 BinarySearchTree.prototype.insert = function(value) {
   var node = new BinarySearchTree(value);
 
-  //recursive function:
-
-  // if (value > node.root) {
-  //   console.log('true', value);
-  //   if (this.right === undefined) {
-  //     this.right = node;
-  //   } 
-  //   else if (this.right !== undefined) {
-  //     if (value > this.right.value) {
-  //       if (this.right.right === undefined) {
-  //         this.right.right = node;
-  //         node.root = this.right.value;
-  //       } 
-  //     } 
-  //     else if (value < this.right.value) {
-  //       if (this.right.left === undefined) {
-  //         this.right.left = node;
-  //         node.root = this.right.value;
-  //       }
-  //     }
-  //   }
-
-  // } else if (value < node.root) {
-  //   console.log('true less than', value)
-  //   if (this.left === undefined) {
-  //     this.left = node;
-  //     console.log('this.left', this.left)
-  //   } else if (this.left !== undefined) {
-  //     if (value > this.left.value) {
-  //       if (this.left.right === undefined) {
-  //         this.left.right = node;
-  //         console.log('this.left.right', this.left.right)
-  //       } 
-  //     } else if (value < this.left.value) {
-  //       if (this.left.left === undefined) {
-  //         this.left.left = node;
-  //         console.log('this.left.left', this.left.left)
-  //       }
-  //     }
-  //   } 
-  // }
   if (node.value > this.value) {
     if (this.right === undefined) {
       this.right = node;
@@ -66,38 +25,37 @@ BinarySearchTree.prototype.insert = function(value) {
 
 BinarySearchTree.prototype.contains = function(value) {
   
-  if (this.value === value) {
-    return true;
-  } else if (value > this.value) {
-    if (this.right.value === value) {
+    if (this.value === value) {
       return true;
     }
-  } else if (value < this.value) {
-    if (this.left.value === value) {
-      return true;
+    else if (value > this.value && this.right) {
+      return this.right.contains(value);
     }
-  }
-  return false;
-};
+    else if (value < this.value && this.left) {
+      return this.left.contains(value);
+    }
+   
+   return false; 
+
+
+};  
+
+  // if (this.value === value) {
+  //   return true;
+  // } else if (value > this.value) {
+  //   if (this.right.value === value) {
+  //     return true;
+  //   }
+  // } else if (value < this.value) {
+  //   if (this.left.value === value) {
+  //     return true;
+  //   }
+  // }
+  // return false;
+
 
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
 //recursively iterate over all nodes and call a function on them;
-  
-//pass over each value
-//callback of each value
-// node = node || this;
-
-// var recursiveFunction = function(node) {
-//   if (this.value !== undefined) {
-//     callback(node.value);
-//     if (this.left !== undefined) {
-//       recursiveFunction(this.left);
-//     } if (this.right !== undefined) {
-//       recursiveFunction(this.right);
-//     }
-// } 
-// }
-// recursiveFunction(this);
 
   callback(this.value);
   if (this.left !== undefined) {
